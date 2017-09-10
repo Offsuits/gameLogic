de282676471e273693b1c0c694850e9946a3ef8e
Easak Hong <easakhong@gmail.com>
2017-09-09 21:23:27 -0700

    onelogic

--- pokerGameLogic.js
+++ pokerGameLogic.js
@@ -1,6 +1,11 @@
 var Hand = require('pokersolver').Hand;
 const express = require('express');
 const app = express();
-app.listen(4000, function () {
-  console.log('Example app listening on port 4000!')
+
+app.get('holeCards', (req, res)=>{
+  res.send('5h5s')
+})
+
+app.listen(4001, function () {
+  console.log('Example app listening on port 4001!')
 });
